3c1b0ef406744676d47551094eb79bd3f3a15e6f
pkill-preston <0v3rthinker@protonmail.com>
2021-03-23 08:24:06 -0300

    Definitive Script

---
+++ assets/script/main.js
@@ -0,0 +1,19 @@
+const open = document.getElementById('openModal');
+const close = document.getElementById('closeModal');
+const modal = document.getElementById('modalContainer');
+const button = document.querySelector('#closeButton');
+
+open.addEventListener("click", () =>
+{
+    modal.classList.add("active");
+});
+
+close.addEventListener("click", () =>
+{
+    modal.classList.remove("active");
+});
+
+button.addEventListener("click", () =>
+{
+    modal.classList.remove("active");
+});
